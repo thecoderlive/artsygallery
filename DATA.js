f55351b4f310fa53a981f6cc3216f5209666aa97
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-02-02 18:05:11 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,40 @@
+export const item = {
+    "id": "24902919-fca4-4e6e-89c9-d91271f8466f",
+    "artsy_gallery_list": [
+        {
+            "id": "0fa34e1a-2703-4e2e-8c6d-83b4f4c9d382",
+            "pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/160.png",
+            "pic_name": "Claim deal television floor central strategy necessary. Act near memory fast nearly young. Brother write production likely blood partner.",
+            "company_name": "machine",
+            "date": "television"
+        },
+        {
+            "id": "55be526f-fb3e-4bfe-b191-671b12d2d899",
+            "pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/250.png",
+            "pic_name": "Issue admit arrive this establish kid spring. One skin age support possible since. Including them wait color.",
+            "company_name": "lawyer",
+            "date": "I"
+        },
+        {
+            "id": "b5f4d7f3-b1d9-47cb-a0ed-cc84be686b08",
+            "pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/140.png",
+            "pic_name": "All marriage special state field. Brother factor become whose town himself. Arm maintain recognize white.",
+            "company_name": "series",
+            "date": "weight"
+        },
+        {
+            "id": "23f7e5ae-a37d-48c5-9e41-fa9d282b19f5",
+            "pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/344.png",
+            "pic_name": "West do reason enough reflect order. Type score opportunity drop much.",
+            "company_name": "reflect",
+            "date": "benefit"
+        },
+        {
+            "id": "f3c7806e-9508-4dc1-876f-a89d4c3de35a",
+            "pic": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/184.png",
+            "pic_name": "Wall wrong wall report science certain. Issue allow find rise color account test north.",
+            "company_name": "dream",
+            "date": "positive"
+        }
+    ]
+}
